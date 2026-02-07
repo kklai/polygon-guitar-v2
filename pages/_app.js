750acfd67b2672c4ed42ccc599774907fef6b114
Pak Tak Tam <Kermit@Paks-MacBook-Pro.local>
2026-02-07 16:07:21 +0800

    Add cache control meta tags and fix transposeChordLine

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import '@/styles/globals.css'
 import { AuthProvider } from '@/contexts/AuthContext'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   )
