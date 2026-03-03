49245a2ce05802a29486fe97f450f74ffcc347dd
Polygon Admin <admin@polygon.guitars>
2026-03-03 09:30:03 +0800

    Fix import path in tech-stack page

--- pages/admin/tech-stack.js
+++ pages/admin/tech-stack.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Layout from '/components/Layout'
+import Layout from '@/components/Layout'
 import { 
   Database, Server, Cloud, Code, FileCode, Music, 
   Search, CreditCard, TrendingUp, Shield, Settings,
